dcedcc6e79ef9fd853f43eced61abf082282664b
zlargon <zlargon1988@gmail.com>
2019-05-18 10:22:11 -0400

    modify columns' data type

--- sql/import_from_csv.sql
+++ sql/import_from_csv.sql
@@ -4,23 +4,23 @@
 -- create table
 drop table if exists crime;
 create table crime (
-    `INCIDENT_NUMBER`         char(10)
-    , `OFFENSE_CODE`          int
-    , `OFFENSE_CODE_GROUP`    varchar(100)
-    , `OFFENSE_DESCRIPTION`   varchar(100)
-    , `DISTRICT`              char(3)
-    , `REPORTING_AREA`        int
-    , `SHOOTING`              char(1)
-    , `OCCURRED_ON_DATE`      datetime
-    , `YEAR`                  int
-    , `MONTH`                 int
-    , `DAY_OF_WEEK`           varchar(9)
-    , `HOUR`                  int
-    , `UCR_PART`              varchar(20)
-    , `STREET`                varchar(100)
-    , `Lat`                   double
-    , `Long`                  double
-    , `Location`              varchar(100)
+    `INCIDENT_NUMBER`       varchar(13),   -- 142052550, I152044642-00, I152028570-01, I140300475-02, I030217815-08, ...
+    `OFFENSE_CODE`          varchar(5),    -- 0724, 00724, ...
+    `OFFENSE_CODE_GROUP`    varchar(100),
+    `OFFENSE_DESCRIPTION`   varchar(100),
+    `DISTRICT`              varchar(3),    -- "", A1, A7, A15, B2, B3, C6, C11, D4, D14, E5, E13, E18
+    `REPORTING_AREA`        varchar(3),    -- "", 000, ...
+    `SHOOTING`              char(1),       -- "", Y
+    `OCCURRED_ON_DATE`      datetime,
+    `YEAR`                  int,           -- 2015 ~ 2018
+    `MONTH`                 int,           -- 1 ~ 12
+    `DAY_OF_WEEK`           varchar(9),    -- Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+    `HOUR`                  int,           -- 0 ~ 23
+    `UCR_PART`              char(10),      -- "", Part One, Part Two, Part Three, Other
+    `STREET`                varchar(100),
+    `Lat`                   double,        -- -1, ...
+    `Long`                  double,        -- -1, ...
+    `Location`              varchar(100)   -- "(-1,-1)", ...
 );
 
 -- enable local_infile
